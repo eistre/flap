8ae665973789d653ff403f2d981d3efc47f02fe7
Eistre <taavi30eistre@gmail.com>
2021-11-10 12:11:34 +0200

    ver1.0 working, added collision and scoring

--- projekt.py
+++ projekt.py
@@ -3,9 +3,9 @@ import sys
 import random
 
 def tekita_värav():
-    punkt = random.randint(300, 700)
-    alumine_värav = värav.get_rect(midtop = (700, punkt))
-    ülemine_värav = värav.get_rect(midbottom = (700, punkt - 250))
+    punkt = random.randint((ekraan_y * 0.42) // 1 , (ekraan_y * 0.85) // 1)
+    alumine_värav = värav.get_rect(midtop = (ekraan_x + 100, punkt))
+    ülemine_värav = värav.get_rect(midbottom = (ekraan_x + 100, punkt - random.randint(200, 300)))
     return alumine_värav, ülemine_värav
 
 def liiguta_värav(väravad):
@@ -19,8 +19,11 @@ def joonista_värav(väravad):
     return väravad
 
 # algsätted
+ekraan_x = 600
+ekraan_y = 800
+
 pygame.init()
-ekraan = pygame.display.set_mode((600, 800))
+ekraan = pygame.display.set_mode((ekraan_x, ekraan_y))
 fps = pygame.time.Clock()
 värava_tekkimine = pygame.USEREVENT
 pygame.time.set_timer(värava_tekkimine, 1200)
@@ -33,20 +36,38 @@ taust = pygame.image.load('images/background.png').convert()
 põrand = pygame.image.load('images/põrand.png').convert()
     # mängija pilt ja hitbox
 mängija = pygame.image.load('images/kast.png').convert()
-mängija_rect = mängija.get_rect(center = (150, 400))
+mängija_rect = mängija.get_rect(center = (150, ekraan_y // 2))
     # väravad
 värav = pygame.image.load('images/värav.png').convert()
 väravad = []
 
+# loeb txt failist high skoori
+f = open('high.txt')
+high_skoor = int(f.readline().strip())
+f.close()
+skoor = 0
+font = pygame.font.SysFont('Arial', 100)
+text_high_skoor = font.render(f'High score: {high_skoor}', True, (255, 0, 0))
+text_skoor = font.render(f'{int(skoor)}', True, (255, 0, 0))
+
+mäng = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit() # ilma sys.exit() tekitab veateate
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and mäng:
                 liikumine = 0
                 liikumine -= 10
+            elif event.key == pygame.K_SPACE and mäng == False:
+                väravad.clear()
+                mängija_rect = mängija.get_rect(center = (150, ekraan_y // 2))
+                liikumine = 0
+                skoor = 0
+                text_skoor = font.render(f'{int(skoor)}', True, (255, 0, 0))
+                mäng = True
+                
         if event.type == värava_tekkimine:
             # iga 1.2 sekundi tagant tekitatakse uus paar väravaid
             väravad.extend(tekita_värav())
@@ -54,17 +75,47 @@ while True:
     # ekraanile ilmub taust
     ekraan.blit(taust, (0, 0))
     
-    # mängija liikumine
-    liikumine += 0.3
-    mängija_rect.centery += liikumine
-    ekraan.blit(mängija, mängija_rect)
     
-    # väravad
-    väravad = liiguta_värav(väravad)
-    joonista_värav(väravad)
+    if mäng:
+        # mängija liikumine
+        liikumine += 0.3
+        mängija_rect.centery += liikumine
+        ekraan.blit(mängija, mängija_rect)
+        
+        # väravad
+        väravad = liiguta_värav(väravad)
+        joonista_värav(väravad)
+        ekraan.blit(text_skoor, (ekraan_x // 2 - 33, ekraan_y // 8))
+        # collision väravatega
+        for i in väravad:
+            if pygame.Rect.colliderect(mängija_rect, i):
+                mäng = False
+            if mängija_rect.centerx in list(range(i.centerx-2, i.centerx+2)):
+                skoor += 0.5
+                text_skoor = font.render(f'{int(skoor)}', True, (255, 0, 0))
+        # collision põranda, katusega
+        if mängija_rect.top <= 0 or mängija_rect.bottom >= ekraan_y - 80:
+            mäng = False
+        
+    else:
+        # prindib high skoori ja mängus saavutatud skoori
+        # kui skoor on suurem siis kirjutatakse high skoor üle
+        ekraan.blit(mängija, (150, ekraan_x // 2))
+        if skoor > high_skoor:
+            high_skoor = int(skoor)
+            text_high_skoor = font.render(f'High score: {high_skoor}', True, (255, 0, 0))
+            
+            f = open('high.txt', 'w')
+            f.write(str(high_skoor))
+            f.close()
+            
+        ekraan.blit(text_high_skoor, (10, 10))
+        ekraan.blit(text_skoor, (ekraan_x // 2 - 33, ekraan_y // 8))
+        
+        
     
     # ekraanile ilmub põrand
-    ekraan.blit(põrand, (0, 720))
+    ekraan.blit(põrand, (0, ekraan_y - 80))
     
     pygame.display.update()
     fps.tick(90)
